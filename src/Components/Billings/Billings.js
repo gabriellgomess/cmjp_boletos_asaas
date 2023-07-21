aaf05c8d045cf14491102ecca6771d38bd11b531
gabriellgomess <51470523+gabriellgomess@users.noreply.github.com>
2023-07-21 15:17:56 -0300

    emissao de relatorio csv

--- src/Components/Billings/Billings.js
+++ src/Components/Billings/Billings.js
@@ -1,160 +1,240 @@
 import React from "react";
-import { 
-    Box,
-    Typography,     
-    Card, 
-    CardContent ,
-    FormControl,
-    InputLabel,
-    Select,
-    MenuItem,
-    Button,
-    Accordion,
-    AccordionSummary,
-    AccordionDetails,
-    TextField,
-    Skeleton,
-    LinearProgress
-} from '@mui/material';
+import {
+  Box,
+  Typography,
+  Card,
+  CardContent,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  TextField,
+  Skeleton,
+  LinearProgress,
+} from "@mui/material";
 
-import axios from 'axios';
+import axios from "axios";
 
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-import { useForm, Controller } from 'react-hook-form'; 
+import { useForm, Controller } from "react-hook-form";
 
 import { useTheme } from "@mui/material/styles";
 
 import TableBillings from "../TableBillings/TableBillings";
 
 const Billings = () => {
-    const { control, handleSubmit, register, reset, formState: { errors } } = useForm();
-    const theme = useTheme();
-    const [billings, setBillings] = React.useState([]);
-    const [loading, setLoading] = React.useState(false);
+  const {
+    control,
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const theme = useTheme();
+  const [billings, setBillings] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
 
-    const onSubmit = (data) => {
-        setBillings([]);
-        console.log(data);
-        setLoading(true);
-        axios.post(`${process.env.REACT_APP_URL}/asaas.php?param=11`, data)
-            .then(response => {
-                console.log(response.data.data);
-                setBillings(response.data.data);
-                setLoading(false);
-            }
-        );
-    }
+  const onSubmit = (data) => {
+    setBillings([]);
+    console.log(data);
+    setLoading(true);
+    axios
+      .post(`${process.env.REACT_APP_URL}/asaas.php?param=11`, data)
+      .then((response) => {
+        console.log(response.data.data);
+        setBillings(response.data.data);
+        setLoading(false);
+      });
+  };
 
-    const total = billings.reduce((acc, billing) => {
-        return acc + billing.value;
-    }, 0);
+  const total = billings.reduce((acc, billing) => {
+    return acc + billing.value;
+  }, 0);
 
-    const handleClear = () => {
-        reset();
-    }
-    return (
-        <Box>
-             <Accordion>
-                <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-                >
-                <Typography variant='h5'>Filtros</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <Box sx={{display: 'flex', flexWrap: 'wrap', gap: '15px'}}>
-                            <FormControl sx={{width: {xs: '100%', sm: '100%', md: '49%'}}}>
-                                <InputLabel id="demo-simple-select-label">Forma de Pagamento</InputLabel>
-                                <Controller
-                                    name="billingType"
-                                    control={control}
-                                    defaultValue=""
-                                    render={({ field }) => (
-                                        <Select
-                                            {...field}
-                                            labelId="demo-simple-select-label"
-                                            id="demo-simple-select"
-                                            label="Forma de Pagamento"
-                                        >
-                                            <MenuItem value="BOLETO">Boleto</MenuItem>
-                                            <MenuItem value="CREDIT_CARD">Cartão de Crédito</MenuItem>
-                                            <MenuItem value="PIX">Pix</MenuItem>
-                                            <MenuItem value="UNDEFINED">Usuário define</MenuItem>
-                                        </Select>
-                                    )}
-                                />
-                            </FormControl>
-                            <FormControl sx={{width: {xs: '100%', sm: '100%', md: '49%'}}}>
-                                <InputLabel id="demo-simple-select-label">Status</InputLabel>
-                                <Controller
-                                    name="status"
-                                    control={control}
-                                    defaultValue=""
-                                    render={({ field }) => (
-                                        <Select
-                                            {...field}
-                                            labelId="demo-simple-select-label"
-                                            id="demo-simple-select"
-                                            label="Status"
-                                        >
-                                            <MenuItem value="PENDING">Aguardando pagamento</MenuItem>
-                                            <MenuItem value="RECEIVED">Recebida (saldo já creditado na conta)</MenuItem>
-                                            <MenuItem value="CONFIRMED">Pagamento confirmado (saldo ainda não creditado)</MenuItem>
-                                            <MenuItem value="OVERDUE">Vencida</MenuItem>
-                                        </Select>
-                                    )}
-                                />
-                            </FormControl>
-                            <Box sx={{border: `1px solid ${theme.palette.accent.light}`, borderRadius: '8px', padding: '10px', width: '32%'}}>
-                                <Typography variant="subtitle1">Data de Criação</Typography>
-                                <Box sx={{display: 'flex', gap: '10px'}}>
-                                   <TextField type="date" {...register("createdDateInicial")} label="De" InputLabelProps={{ shrink: true }}/> 
-                                   <TextField type="date" {...register("createdDateFinal")} label="Até" InputLabelProps={{ shrink: true }}/> 
-                                </Box>                                
-                            </Box>
-                            <Box sx={{border: `1px solid ${theme.palette.accent.light}`, borderRadius: '8px', padding: '10px', width: '32%'}}>
-                                <Typography variant="subtitle1">Data de Vencimento</Typography>
-                                <Box sx={{display: 'flex', gap: '10px'}}>
-                                   <TextField type="date" {...register("dueDatenicial")} label="De" InputLabelProps={{ shrink: true }}/> 
-                                   <TextField type="date" {...register("dueDateFinal")} label="Até" InputLabelProps={{ shrink: true }}/> 
-                                </Box>                                
-                            </Box>
-                            <Box sx={{border: `1px solid ${theme.palette.accent.light}`, borderRadius: '8px', padding: '10px', width: '32%'}}>
-                                <Typography variant="subtitle1">Data de Pagamento</Typography>
-                                <Box sx={{display: 'flex', gap: '10px'}}>
-                                   <TextField type="date" {...register("receivedDateInicial")} label="De" InputLabelProps={{ shrink: true }}/> 
-                                   <TextField type="date" {...register("receivedDateFinal")} label="Até" InputLabelProps={{ shrink: true }}/> 
-                                </Box>                                
-                            </Box>                            
-                        </Box>                        
-                        <Button sx={{marginTop: '15px'}} variant='contained' type="submit">Filtrar</Button>
-                        <Button sx={{marginTop: '15px', marginLeft: '15px'}} variant='outlined' onClick={()=>handleClear()}>Limpar</Button>
-                    </form>
-                </AccordionDetails>
-            </Accordion>
-            {billings.length > 0 ?(
-                <>
-                <TextField sx={{marginTop: '15px'}} disabled label="Total" value={total.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}/>
-                <TableBillings billings={billings}/>                
-                </>              
-                
-            ):(
-                null
-            )}
-            {loading?(
-                <Box sx={{width: '100%'}}>
-                <LinearProgress />
+  const handleClear = () => {
+    reset();
+  };
+  return (
+    <Box>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography variant="h5">Filtros</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box sx={{ display: "flex", flexWrap: "wrap", gap: "15px" }}>
+              <FormControl
+                sx={{ width: { xs: "100%", sm: "100%", md: "49%" } }}
+              >
+                <InputLabel id="demo-simple-select-label">
+                  Forma de Pagamento
+                </InputLabel>
+                <Controller
+                  name="billingType"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      label="Forma de Pagamento"
+                    >
+                      <MenuItem value="BOLETO">Boleto</MenuItem>
+                      <MenuItem value="CREDIT_CARD">Cartão de Crédito</MenuItem>
+                      <MenuItem value="PIX">Pix</MenuItem>
+                      <MenuItem value="UNDEFINED">Usuário define</MenuItem>
+                    </Select>
+                  )}
+                />
+              </FormControl>
+              <FormControl
+                sx={{ width: { xs: "100%", sm: "100%", md: "49%" } }}
+              >
+                <InputLabel id="demo-simple-select-label">Status</InputLabel>
+                <Controller
+                  name="status"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      label="Status"
+                    >
+                      <MenuItem value="PENDING">Aguardando pagamento</MenuItem>
+                      <MenuItem value="RECEIVED">
+                        Recebida (saldo já creditado na conta)
+                      </MenuItem>
+                      <MenuItem value="CONFIRMED">
+                        Pagamento confirmado (saldo ainda não creditado)
+                      </MenuItem>
+                      <MenuItem value="OVERDUE">Vencida</MenuItem>
+                    </Select>
+                  )}
+                />
+              </FormControl>
+              <Box
+                sx={{
+                  border: `1px solid ${theme.palette.accent.light}`,
+                  borderRadius: "8px",
+                  padding: "10px",
+                  width: "32%",
+                }}
+              >
+                <Typography variant="subtitle1">Data de Criação</Typography>
+                <Box sx={{ display: "flex", gap: "10px" }}>
+                  <TextField
+                    type="date"
+                    {...register("createdDateInicial")}
+                    label="De"
+                    InputLabelProps={{ shrink: true }}
+                  />
+                  <TextField
+                    type="date"
+                    {...register("createdDateFinal")}
+                    label="Até"
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Box>
+              </Box>
+              <Box
+                sx={{
+                  border: `1px solid ${theme.palette.accent.light}`,
+                  borderRadius: "8px",
+                  padding: "10px",
+                  width: "32%",
+                }}
+              >
+                <Typography variant="subtitle1">Data de Vencimento</Typography>
+                <Box sx={{ display: "flex", gap: "10px" }}>
+                  <TextField
+                    type="date"
+                    {...register("dueDatenicial")}
+                    label="De"
+                    InputLabelProps={{ shrink: true }}
+                  />
+                  <TextField
+                    type="date"
+                    {...register("dueDateFinal")}
+                    label="Até"
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Box>
+              </Box>
+              <Box
+                sx={{
+                  border: `1px solid ${theme.palette.accent.light}`,
+                  borderRadius: "8px",
+                  padding: "10px",
+                  width: "32%",
+                }}
+              >
+                <Typography variant="subtitle1">Data de Pagamento</Typography>
+                <Box sx={{ display: "flex", gap: "10px" }}>
+                  <TextField
+                    type="date"
+                    {...register("receivedDateInicial")}
+                    label="De"
+                    InputLabelProps={{ shrink: true }}
+                  />
+                  <TextField
+                    type="date"
+                    {...register("receivedDateFinal")}
+                    label="Até"
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Box>
+              </Box>
             </Box>
-            ):(
-                null
-            )}
-            
-           
+            <Button
+              sx={{ marginTop: "15px" }}
+              variant="contained"
+              type="submit"
+            >
+              Filtrar
+            </Button>
+            <Button
+              sx={{ marginTop: "15px", marginLeft: "15px" }}
+              variant="outlined"
+              onClick={() => handleClear()}
+            >
+              Limpar
+            </Button>
+          </form>
+        </AccordionDetails>
+      </Accordion>
+      {billings.length > 0 ? (
+        <>
+          <TextField
+            sx={{ marginTop: "15px" }}
+            disabled
+            label="Total"
+            value={total.toLocaleString("pt-br", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          />
+          <TableBillings billings={billings} />
+        </>
+      ) : null}
+      {loading ? (
+        <Box sx={{ width: "100%" }}>
+          <LinearProgress />
         </Box>
-    );
-}
+      ) : null}
+    </Box>
+  );
+};
 
 export default Billings;
--- src/Components/TableBillings/TableBillings.js
+++ src/Components/TableBillings/TableBillings.js
@@ -1,94 +1,189 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import { DataGrid, ptBR  } from '@mui/x-data-grid';
-import { Typography, Button } from '@mui/material';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { DataGrid, ptBR,  GridToolbar } from "@mui/x-data-grid";
+import { Typography, Button } from "@mui/material";
 
-const columns = [  
+const columns = [
   {
-    field: 'customer',
-    headerName: 'ID do Doador',
+    field: "customerName",
+    headerName: "Nome do Doador",
+    width: 200,
+    editable: true,
+  },
+  {
+    field: "cpfCnpj",
+    headerName: "CPF/CNPJ",
+    width: 120,
+    editable: true,
+  },
+  {
+    field: "email",
+    headerName: "E-mail",
     width: 150,
     editable: true,
   },
   {
-    field: 'dueDate',
-    headerName: 'Vencimento',
-    width: 150,
+    field: "phone",
+    headerName: "Telefone",
+    width: 120,
+    editable: true,
+  },
+  {
+    field: "mobilePhone",
+    headerName: "Celular",
+    width: 120,
+    editable: true,
+  },
+  {
+    field: "postalService",
+    headerName: "Correio",
+    width: 70,
     editable: true,
     renderCell: (params) => (
-        <Typography variant="body1" color="text.secondary" component="div">
-          {params.row.dateCreated.split("-").reverse().join("/")}
-        </Typography>
-      ),
+      <Typography variant="body1" color="text.secondary" component="div">
+        {params.row.postalService ? "Sim" : "Não"}
+      </Typography>
+    ),
   },
   {
-    field: 'value',
-    headerName: 'Valor',
+    field: "dueDate",
+    headerName: "Vencimento",
+    width: 100,
+    editable: true,
+    renderCell: (params) => (
+      <Typography variant="body1" color="text.secondary" component="div">
+        {params.row.dateCreated.split("-").reverse().join("/")}
+      </Typography>
+    ),
+  },
+  {
+    field: "value",
+    headerName: "Valor",
     width: 110,
     editable: true,
     renderCell: (params) => (
-        <Typography variant="body1" color="text.secondary" component="div">
-          {params.row.value.toLocaleString("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          })}
-        </Typography>
-      ),
-    
+      <Typography variant="body1" color="text.secondary" component="div">
+        {params.row.value.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </Typography>
+    ),
   },
   {
-    field: 'billingType',
-    headerName: 'Forma de Pagamento',
-    width: 150,
+    field: "billingType",
+    headerName: "Método",
+    width: 80,
     editable: true,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 200,
+    editable: true,
+    renderCell: (params) => {
+      let status = params.value; // assumindo que o status está no valor do params
+      let message;
+  
+      switch (status) {
+        case 'PENDING':
+            message = "Aguardando pagamento";
+            break;
+        case 'RECEIVED':
+            message = "Recebida (saldo já creditado na conta)";
+            break;
+        case 'CONFIRMED':
+            message = "Pagamento confirmado (saldo ainda não creditado)";
+            break;
+        case 'OVERDUE':
+            message = "Vencida";
+            break;
+        case 'REFUNDED':
+            message = "Estornada";
+            break;
+        case 'RECEIVED_IN_CASH':
+            message = "Recebida em dinheiro (não gera saldo na conta)";
+            break;
+        case 'REFUND_REQUESTED':
+            message = "Estorno Solicitado";
+            break;
+        case 'REFUND_IN_PROGRESS':
+            message = "Estorno em processamento (liquidação já está agendada, cobrança será estornada após executar a liquidação)";
+            break;
+        case 'CHARGEBACK_REQUESTED':
+            message = "Recebido chargeback";
+            break;
+        case 'CHARGEBACK_DISPUTE':
+            message = "Em disputa de chargeback (caso sejam apresentados documentos para contestação)";
+            break;
+        case 'AWAITING_CHARGEBACK_REVERSAL':
+            message = "Disputa vencida, aguardando repasse da adquirente";
+            break;
+        case 'DUNNING_REQUESTED':
+            message = "Em processo de negativação";
+            break;
+        case 'DUNNING_RECEIVED':
+            message = "Recuperada";
+            break;
+        case 'AWAITING_RISK_ANALYSIS':
+            message = "Pagamento em análise";
+            break;
+        default:
+            message = `Status desconhecido: ${status}`;
+      }
+  
+      return <Typography variant="body1" color="text.secondary" component="div">{message}</Typography>;
     },
-    {
-        field: 'invoiceUrl',
-        headerName: 'Link do Pagamento',
-        width: 150,
-        editable: true,
-        renderCell: (params) => (
-            <Button href={params.row.invoiceUrl} target='blank'>
-                Link
-            </Button>
-          ),
-    },
-    // {
-    //     field: 'actions',
-    //     headerName: 'Ações',
-    //     width: 150,
-    //     editable: true,
-    //     renderCell: (params) => (
-    //         <Button>
-    //             Ver
-    //         </Button>
-    //       ),
-    // },
+  },
+  
+  {
+    field: "invoiceUrl",
+    headerName: "Link",
+    width: 100,
+    editable: true,
+    renderCell: (params) => (
+      <Button href={params.row.invoiceUrl} target="blank">
+        Link
+      </Button>
+    ),
+  },
+  // {
+  //     field: 'actions',
+  //     headerName: 'Ações',
+  //     width: 150,
+  //     editable: true,
+  //     renderCell: (params) => (
+  //         <Button>
+  //             Ver
+  //         </Button>
+  //       ),
+  // },
 ];
 
 export default function TableBillings(props) {
   return (
-    <Box sx={{ height: 400, width: '100%', marginTop: '15px' }}>
-    {props.billings.length > 0 ? (
-      <DataGrid
-      localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
-        rows={props.billings}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 10,
+    <Box sx={{ height: 400, width: "100%", marginTop: "15px" }}>
+      {props.billings.length > 0 ? (
+        <DataGrid
+          localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
+          rows={props.billings}
+          columns={columns}
+          slots={{ toolbar: GridToolbar }}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 10,
+              },
             },
-          },
-        }}
-        pageSizeOptions={[5, 10, 20, 50, 100]}
-        disableRowSelectionOnClick
-      />
-    ) : (
+          }}
+          pageSizeOptions={[5, 10, 20, 50, 100]}
+          disableRowSelectionOnClick
+        />
+      ) : (
         <Typography variant="body1" color="text.secondary" component="div">
-            Nenhum dado foi retornado para a pesquisa realizada.
+          Nenhum dado foi retornado para a pesquisa realizada.
         </Typography>
-    )}
+      )}
     </Box>
   );
 }
